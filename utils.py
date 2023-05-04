f68492ffa1bcfa6565f13a96c6d1d88ac19499bf
Zack Lee <zlee@carnegielearning.com>
2023-05-04 03:02:38 -0400

    Add files

---
+++ utils.py
@@ -0,0 +1,81 @@
+import os
+import tiktoken
+
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+SYSTEM_PROMPT = '''
+You are an AI assistant. 
+You will answer the question as truthfully as possible.
+If you're unsure of the answer, say Sorry, I don't know.
+'''
+WAIT_MESSAGE = "Got your request. Please wait."
+N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 20
+MAX_TOKENS = 8192
+
+
+# From https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+def num_tokens_from_messages(messages, model="gpt-4"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+        return num_tokens_from_messages(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+def process_conversation_history(conversation_history, bot_user_id):
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+    for message in conversation_history["messages"][:-1]:
+        role = "assistant" if message["user"] == bot_user_id else "user"
+        message_text = process_message(message, bot_user_id)
+        if message_text:
+            messages.append({"role": role, "content": message_text})
+    return messages
+
+
+def process_message(message, bot_user_id):
+    message_text = message["text"]
+    role = "assistant" if message["user"] == bot_user_id else "user"
+    message_text = clean_message_text(message_text, role, bot_user_id)
+    return message_text
+
+
+def clean_message_text(message_text, role, bot_user_id):
+    if (f"<@{bot_user_id}>" in message_text) or (role == "assistant"):
+        message_text = message_text.replace(f"<@{bot_user_id}>", "").strip()
+        return message_text
+    return None
+
+
+def update_chat(app, channel_id, reply_message_ts, response_text):
+    app.client.chat_update(
+        channel=channel_id,
+        ts=reply_message_ts,
+        text=response_text
+    )
+
